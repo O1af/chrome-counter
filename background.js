9e73c4dbf82d2425110598a06115797dc2a4e171
Olaf Dsouza <olafdsouza27@gmail.com>
2022-01-21 18:30:54 -0500

    V 0.2 added tab count functionality

--- background.js
+++ background.js
@@ -1,4 +1,17 @@
 const getTabCount = async () => {
-	const tabs = await browser.tabs.query({});
+	const tabs = await chrome.tabs.query({});
 	return tabs.length;
 };
+
+chrome.tabs.onCreated.addListener(async () => {
+	const count = await getTabCount();
+	chrome.action.setBadgeText({ text: count.toString() });
+});
+chrome.tabs.onRemoved.addListener(async () => {
+	const count = await getTabCount();
+	chrome.action.setBadgeText({ text: count.toString() });
+});
+chrome.runtime.onInstalled.addListener(async () => {
+	const count = await getTabCount();
+	chrome.action.setBadgeText({ text: count.toString() });
+});
